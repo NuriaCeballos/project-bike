17e9c86c9428b0c178679ad3f867afffeba32a78
Nuria <71881401+NuriaCeballos@users.noreply.github.com>
2021-07-21 10:01:40 +0200

    primer commit en VUE

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Hola</router-link> |
+    <router-link to="/about">Adios</router-link>
   </div>
   <router-view/>
 </template>
