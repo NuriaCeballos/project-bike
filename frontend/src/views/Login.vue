ad476276200d4c13f9e5ac7c595c8f940e48a00b
Nuria <71881401+NuriaCeballos@users.noreply.github.com>
2021-07-22 15:16:04 +0200

    componente login

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="" src="">
+    <Box msg="aquí va la caja de login"/>
+  </div>
+</template>
+
+
+<script>
+import Box from '@/components/Box.vue'
+export default {
+    name:'Login',
+    components:{
+        Box
+    }
+
+}
+</script>
+
+<style>
+
+</style>
