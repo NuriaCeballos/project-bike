5dea44fad8042cb92c68e82007b7a76257179b5f
Nuria <71881401+NuriaCeballos@users.noreply.github.com>
2021-08-03 14:19:44 +0200

    comunicación entre componentes

--- frontend/src/components/Store.vue
+++ frontend/src/components/Store.vue
@@ -1,20 +1,54 @@
 <template>
+<div>
   <div class="prueba">
+      <h1>Store</h1>
       <input v-model="nombre" placeholder="nombre">
       <input v-model="apellidos" placeholder="apellidos">
-    <h1>Store</h1>
-    <div><Userdata/></div>
+      <div>
+      <button class="btn btn-primary" @click="login">Login</button>
+      <button class="btn btn-danger" @click="logout">Logout</button>
+      <button class="btn btn-warning" @click="aleatorio">Aleatorio</button>  
+    </div>
+  
+  <div><Userdata/></div>
+  </div>
   </div>
 </template>
 <script>
 
 import Userdata from '@/components/Userdata'
+import {ref,reactive,computed} from 'vue'
+
 export default {
     name:'Store',
     components: {
       Userdata
+       
     },
     props:{},
+    setup(){
+        let nombre=ref("")
+        let apellidos= ref("")
+        function login(){
+            
+
+        }
+        function logout(){
+            
+        }
+        function aleatorio(){
+            
+        }
+
+        return{
+            nombre,
+            apellidos,
+            login,
+            logout,
+            aleatorio
+        }
+
+    }
     
     
 }
@@ -23,7 +57,12 @@ export default {
 
 <style lang="scss" scoped>
   .prueba{
-    background: aqua;
+    background: rgb(113, 161, 161);
+    border-radius: 20px;
+    
+  }
+  button,input {
+    margin:10px;
   }
  
   
--- frontend/src/components/Userdata.vue
+++ frontend/src/components/Userdata.vue
@@ -1,15 +1,20 @@
 <template>
 
-    <h3>Hola soy Userdata</h3>
+   
+    <h3>{{nombre}} {{apellidos}} Userdata</h3>
     
 </template>
 <script>
+import {ref} from 'vue'
 export default {
     name: "Userdata",
     props:{},
     setup(){
+        let nombre=ref('')
+        let apellidos=ref('')
         return{
-
+            nombre,
+            apellidos
         }
     }
     
