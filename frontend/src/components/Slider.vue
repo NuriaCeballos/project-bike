a90f342e5055923877c6ce59385601cab71b757a
Nuria <71881401+NuriaCeballos@users.noreply.github.com>
2021-08-03 09:48:35 +0200

    images

--- frontend/src/components/Slider.vue
+++ frontend/src/components/Slider.vue
@@ -1,37 +1,19 @@
 
 <template>
 
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-  <h1>{{texto}}</h1>
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+<div>
+      <img v-for= "image in images" :key="image.id" :src="image.url" class="d-block w-100" :alt="image.name" width="200px">
+    <pre>
+      {{images}}
+    </pre>
+    
 </div>
 
 </template>
 
 <script>
+import {ref} from 'vue'
+
 export default {
      name: 'Slider',
   props: {
@@ -39,8 +21,20 @@ export default {
   },
   setup(){
     let texto= "AQUÍ VA EL SLIDER"
+    const images= ref([
+      {
+      id:1,url: require('../assets/bici1.jpg'), name: 'foto1',
+      },
+       {
+      id:2,url:require('../assets/bici3.jpg'), name: 'foto2',
+      },
+       {
+      id:3,url:require('../assets/bici4.jpg'), name: 'foto3',
+      },
+    ])
       return {
-        texto
+        texto,
+        images
       }
   }
  
