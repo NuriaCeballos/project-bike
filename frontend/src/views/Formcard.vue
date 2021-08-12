c26563b7146996a1c973edce3c0d8c238bd247b6
Nuria <71881401+NuriaCeballos@users.noreply.github.com>
2021-08-12 10:10:55 +0200

    creando formulario con flex-box

--- frontend/src/views/Formcard.vue
+++ frontend/src/views/Formcard.vue
@@ -1,16 +1,18 @@
 <template>
 <div class="form">
-    <form>
+    <form class="form">
+        <div>
         <select>
             <option>Rutas</option>
             <option>Comprar y vender</option>
             <option>Recursos Bike</option>
         </select>
-        <input name="ranking" type="number" placeholder="ranking"><br>
-        <input name="nickname" type="text" placeholder="Nickname">
+        </div>
+        <!-- <input name="ranking" type="number" placeholder="ranking"><br> es otro componente-->
+       <div> <input name="nickname" type="text" placeholder="Nickname"></div>
         <div class="photo">Espacio para la foto</div>
-        <textarea name="description"></textarea>
-        
+        <div><textarea name="description"></textarea></div>
+        <!-- <textarea name="comentarios"></textarea>  es otro componente-->
     </form>
 
 </div>
@@ -18,20 +20,43 @@
 </template>
 
 <script>
+import {ref,reactive} from 'vue'
 export default {
-    name:"Formcard"
+    name:"Formcard",
+    props:{}, 
+    setup(){
+        let tema = reactive["Rutas","Comprar y vender","Recuerdos Bike"]
+        let nickname= ref("")
+        let photo= ref("")
+        let description= ref("")
+        return{
+            tema,nickname,photo,description
+        }
 
+    }   
 }
 </script>
 
 <style lang="scss" scoped>
-div.photo{
-    background: bisque;
-    width: 550px;
-    height: 200px;
-}
-form{
+
+.form{
     display:flex;
+    align-items: center;
+    background: rgb(228, 157, 64);
+    padding:30px;
+    flex-direction: column;
+    margin: 500px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+
+}
+.form div{
+    background-color:white;
+    width: 190px;
+    margin: 30px;
+
+    
+    
 }
 
 </style>
